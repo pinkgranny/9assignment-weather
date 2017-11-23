697e888926c02f9dcdd06ac75e0da7526342c2e4
kmkkkuk <kmkkuk@gmail.com>
2017-11-23 13:58:54 +0100

    Stockholm prints in html

---
+++ code/script.js
@@ -0,0 +1,81 @@
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=3c91b0161e9dafd6c675a2f230453cef").then((response) => {
+  return response.json()
+}).then((json) => {
+  console.log(json)
+  console.log(json.name)
+  document.getElementById("myP").innerHTML = json.name;
+  console.log(json.main.temp)
+  console.log(json.weather[0].main)
+})
+
+
+
+
+
+// document.getElementById("myDIV").innerHTML = "How are you?";
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed!";
+// }
+
+// fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=3c91b0161e9dafd6c675a2f230453cef").then((response) => {
+// return response.json()
+// }).then((json) => {
+// console.log(json.args.pizzaName)
+// })
+
+// <!-- Pizzaexample -->
+// <!-- const pizza = {
+// name: "Pepperoni",
+// date: new Date()
+// }
+
+// console.log(pizza)
+
+// const pizzaString = JSON.stringify(pizza)
+//
+// console.log(pizzaString)
+//
+// const parsedPizzaString = JSON.parse(pizzaString)
+//
+// console.log(parsedPizzaString)
+
+
+// {"coord":
+//
+// {"lon":18.06,"lat":59.33},
+//
+// "weather":[{"id":803,
+// "main":"Clouds",
+// "description":"broken clouds","icon":"04d"}],
+//
+// "base":"stations",
+//   "main":{
+//     "temp":7.47,
+//     "pressure":996,
+//     "humidity":87,
+//     "temp_min":7,
+//     "temp_max":8},
+//
+// "visibility":10000,
+//   "wind":{
+//     "speed":6.2,
+//     "deg":150},
+//
+//   "clouds":{
+//     "all":75},
+//
+// "dt":1511432400,
+//   "sys":{
+//     "type":1,
+//     "id":5420,
+//     "message":0.002,
+//     "country":"SE",
+//     "sunrise":1511420495,
+//     "sunset":1511445988},
+//
+// "id":2673730,
+// "name":"Stockholm",
+// "cod":200
+// }
